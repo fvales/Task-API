118d2efb5aa41b8731d507bdbdbcf9bf2386bb32
Freeda Vales <freedavales28@gmail.com>
2024-09-20 01:37:56 +0530

    Set up controllers

---
+++ controllers/tasks.js
@@ -0,0 +1,19 @@
+export const getAllTasks = (req, res) => {
+  res.send('All Tasks From File!!!');
+};
+
+export const createTask = (req, res) => {
+  res.send('Created Task!!!');
+};
+
+export const getTaskById = (req, res) => {
+  res.send('A Task!!!');
+};
+
+export const updateTask = (req, res) => {
+  res.send('Updated a task!!!');
+};
+
+export const deleteTask = (req, res) => {
+  res.send('Deleted a task!!!');
+};
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import {
+  createTask,
+  deleteTask,
+  getAllTasks,
+  getTaskById,
+  updateTask,
+} from '../controllers/tasks.js';
 
 const taskRoutes = Router();
 
-taskRoutes.get('/', (req, res) => {
-  res.send('All Tasks!!!');
-});
+taskRoutes.get('/', getAllTasks);
+taskRoutes.get('/:id', getTaskById);
+taskRoutes.post('/', createTask);
+taskRoutes.patch('/:id', updateTask);
+taskRoutes.delete('/:id', deleteTask);
 
 export default taskRoutes;
