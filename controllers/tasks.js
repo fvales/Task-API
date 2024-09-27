1bc082c33db90c6e5b513cb117c75c043930b6f3
Freeda Vales <freedavales28@gmail.com>
2024-09-28 00:05:14 +0530

    Create Task - MongoDB

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,17 @@
+import Task from '../models/tasks.js';
+
 export const getAllTasks = (req, res) => {
   res.send('All Tasks From File!!!');
 };
 
-export const createTask = (req, res) => {
-  res.send('Created Task!!!');
+export const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).send({ task });
+  } catch (e) {
+    console.log(e);
+  }
+  res.sendStatus(500);
 };
 
 export const getTaskById = (req, res) => {
